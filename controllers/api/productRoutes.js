2984fa7ea4eec0c9afb9601fd2e6a7fdc006d70e
Molly Salmonsen <mollysalmonsen@Mollys-MacBook-Pro-2.local>
2022-10-06 22:17:22 -0400

    public folder

--- controllers/api/productRoutes.js
+++ controllers/api/productRoutes.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 const { Product } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+// User needs to be logged in before they can post a product
 router.post('/', withAuth, async (req, res) => {
+    // Once logged in they can create a new Product
     try {
       const newProduct = await Product.create({
         ...req.body,
@@ -16,3 +18,32 @@ router.post('/', withAuth, async (req, res) => {
   });
   
   module.exports = router;
+
+//   exports.create = (req, res) => {
+//     // Validate request
+//     if (!req.body.title) {
+//       res.status(400).send({
+//         message: "Content can not be empty!"
+//       });
+//       return;
+//     }
+  
+//     // Create a Tutorial
+//     const tutorial = {
+//       title: req.body.title,
+//       description: req.body.description,
+//       published: req.body.published ? req.body.published : false
+//     };
+  
+//     // Save Tutorial in the database
+//     Tutorial.create(tutorial)
+//       .then(data => {
+//         res.send(data);
+//       })
+//       .catch(err => {
+//         res.status(500).send({
+//           message:
+//             err.message || "Some error occurred while creating the Tutorial."
+//         });
+//       });
+//   };
