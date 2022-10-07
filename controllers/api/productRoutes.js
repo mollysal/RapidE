212650e90967b6718977c48d64ec2c43b2608f23
Molly Salmonsen <mollysalmonsen@Mollys-MacBook-Pro-2.local>
2022-10-06 21:54:55 -0400

    updating homeRoutes.js & productRoutes.js

--- controllers/api/productRoutes.js
+++ controllers/api/productRoutes.js
@@ -2,16 +2,6 @@ const router = require('express').Router();
 const { Product } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-
-router.post('/', withAuth, async (req, res) => {
-    try {
-        const newProduct = await Product.create({
-          ...req.body,
-          user_id: req.session.user_id,
-        });
-    
-});
-
 router.post('/', withAuth, async (req, res) => {
     try {
       const newProduct = await Product.create({
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,31 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const { Product, User } = require('../models');
 const withAuth = require('../utils/auth');
 
+router.get('/', async (req, res) => {
+    try {
+        const dbProductData = await Product.findAll({
+          include: [
+            {
+              model: Product,
+              attributes: ['productName', 'description'],
+            },
+          ],
+        });
+    
+        const prodcuts = dbProductData.map((prodcuts) =>
+          prodcuts.get({ plain: true })
+        );
+        // Send over the 'loggedIn' session variable to the 'homepage' template
+        res.render('homepage', {
+          prodcuts,
+          loggedIn: req.session.loggedIn,
+        });
+      } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+      }
+
+})
 
 module.exports = router;
