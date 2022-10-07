a2305c1b847e522b760a9f091480568491d00e6b
Molly Salmonsen <mollysalmonsen@Mollys-MacBook-Pro-2.local>
2022-10-06 21:59:12 -0400

    update to homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Product, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
+router.get('/', withAuth, async (req, res) => {
     try {
         const dbProductData = await Product.findAll({
           include: [
