a56f210db074a06a7721ee1506aac179c167ecfe
Molly Salmonsen <mollysalmonsen@Mollys-MacBook-Pro-2.local>
2022-10-10 18:54:28 -0400

    working on Index Model

---
+++ models/index.js
@@ -0,0 +1,19 @@
+const User = require('./User');
+const Product = require('./Product');
+const Message = require('./Message');
+
+User.hasMany(Product, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+User.hasMany(Message, {
+    foreignKey: 'user_id',
+});
+
+Product.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+
+module.exports = { User, Product, Message };
