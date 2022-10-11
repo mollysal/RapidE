40246ebbd0e3ea40b5da805f42da3d89fb398f07
Carl Bergquist <carlbergquist81@gmail.com>
2022-10-10 23:28:09 -0500

    login / logout sort of works

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,7 +3,7 @@ const userRoutes = require('./userRoutes');
 const ProductRoutes = require('./ProductRoutes');
 const messageRoutes = require('./messageRoutes');
 
-//router.use('/users', userRoutes);
+router.use('/users', userRoutes);
 //router.use('/prodcuts', ProductRoutes);
 router.use('/messages', messageRoutes);
 
