d99a9d2c73095c2636e9dd4c4c0ee72588de695c
Molly Salmonsen <salmonsen.molly@gmail.com>
2022-10-16 22:42:02 -0400

    trying to work on Form

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,7 +4,7 @@ const ProductRoutes = require('./ProductRoutes');
 const messageRoutes = require('./messageRoutes');
 
 router.use('/users', userRoutes);
-//router.use('/prodcuts', ProductRoutes);
+// router.use('/prodcuts', ProductRoutes);
 router.use('/messages', messageRoutes);
 
 module.exports = router;
